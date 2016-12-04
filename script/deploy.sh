c6b5f338b090b6725731a7d3351f934de0de8e47
@slovacus <slovacus@gmail.com>
2016-12-04 16:04:03 -0500

    [#3] Fix: settings makefile

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,6 +4,10 @@
 # shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
+echo "execute Deploy"
+
+FILE_PLAYBOOK="$PROVISION_DIR/deploy.yaml"
+
 ansible-playbook "${FILE_PLAYBOOK}" -i "${PATH_INVENTORY}"  -v \
                  --user="${USER}"  --private-key="${PRIVATE_KEY}" \
                  --extra-vars "${EXTRA_VARS}" \
